678bb44993e4424f9c04c0a336a8461d08f8d85f
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2017-11-28 22:16:19 +0630

    add ud

--- src/pages/todo/todo.ts
+++ src/pages/todo/todo.ts
@@ -9,14 +9,46 @@ import { TodoProvider as Todo } from '../../providers/todo/todo';
   templateUrl: 'todo.html',
 })
 export class TodoPage {
-  db: Todo;
+  isEditView = false;
+  db: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.db = new Todo();
+    if (this.navParams.data.id) {
+      this.isEditView = true;
+      this.db = this.navParams.data.toJSON();
+      console.log(this.db);
+    } else {
+      this.db = new Todo();
+    }
   }
 
   onCreate() {
     this.db.save().then(
+      data => {
+        this.navCtrl.pop();
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  onUpdate() {
+    let db = new Todo();
+    db.save(this.db).then(
+      data => {
+        this.navCtrl.pop();
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  onDelete() {
+    let db = new Todo();
+    db.id = this.db.objectId;
+    db.destroy().then(
       data => {
         this.navCtrl.pop();
       },
